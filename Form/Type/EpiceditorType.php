13ab984496b322bd63d9fb8fd65888137b44fc7b
backender <marc@mjnet.ch>
2012-12-15 14:12:52 +0100

    Bundle now working
    no configuration possibility yet

--- Form/Type/EpiceditorType.php
+++ Form/Type/EpiceditorType.php
@@ -3,12 +3,9 @@ namespace Backender\EpiceditorBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Form\FormView;
 
-use Symfony\Component\Form\DataTransformerInterface;
 
 /**
  * EpicEditor type
@@ -16,41 +13,32 @@ use Symfony\Component\Form\DataTransformerInterface;
  */
 class EpiceditorType extends AbstractType
 {
-	protected $container;
-	protected $transformers;
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
 
-	public function __construct(ContainerInterface $container)
-	{
-		$this->container = $container;
-	}
-
-
-	/**
-	 * {@inheritdoc}
-	 */
-	public function buildForm(FormBuilderInterface $builder, array $options)
-	{
-		
-		
-	}
-
-	/**
-	 * {@inheritdoc}
-	 */
-	public function buildView(FormView $view, FormInterface $form, array $options)
-	{
-		
-		
-	}
+    }
 
 
-	/**
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+
+    }
+
+
+    /**
 	 * {@inheritdoc}
 	 */
 	public function getParent()
 	{
 		return 'textarea';
 	}
+
 
 	/**
 	 * {@inheritdoc}
